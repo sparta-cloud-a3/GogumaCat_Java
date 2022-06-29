e9bc31434234fe730502011435d71ecc60f5bb8a
kwn1996kr <kwn1996kr@naver.com>
2022-06-29 16:03:57 +0900

    feat: chatting

--- src/main/java/com/example/goguma/controller/ChatRoomController.java
+++ src/main/java/com/example/goguma/controller/ChatRoomController.java
@@ -1,8 +1,10 @@
 package com.example.goguma.controller;
 
 import com.example.goguma.model.ChatRoom;
+import com.example.goguma.model.User;
 import com.example.goguma.security.UserDetailsImpl;
 import com.example.goguma.service.ChatService;
+import com.example.goguma.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -20,11 +22,14 @@ import java.util.Map;
 @RequestMapping("/chat")
 public class ChatRoomController {
     private final ChatService chatService;
+    private final UserService userService;
     private Map<String,Integer> map = new HashMap<String, Integer>();
 
     // 채팅 리스트 화면
     @GetMapping("/room")
-    public String rooms(Model model) {
+    public String rooms(Model model,@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User info = userService.profile(userDetails.getId());
+        model.addAttribute("nickname", info.getNickname());
         return "/room";
     }
 
@@ -44,7 +49,7 @@ public class ChatRoomController {
 
     // 채팅방 입장 화면
     @GetMapping("/room/enter/{roomId}")
-    public String roomDetail(Model model, @PathVariable String roomId, HttpServletResponse response) throws IOException {
+    public String roomDetail(Model model, @PathVariable String roomId, HttpServletResponse response,@AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
         if(map.containsKey(roomId) == false){
             map.put(roomId,1);
         }else{
@@ -56,6 +61,8 @@ public class ChatRoomController {
                 return "/room";
             }
         }
+        User info = userService.profile(userDetails.getId());
+        model.addAttribute("nickname", info.getNickname());
         model.addAttribute("roomId", roomId);
         return "/roomdetail";
     }
