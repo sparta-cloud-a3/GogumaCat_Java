053afdd89fcabbb2fe0374a519e9738d0ed13eb4
hongggs <hongseoda@gmail.com>
2022-07-14 17:18:27 +0900

    add: cors 오류 해결하는 header 추가

--- src/main/java/com/example/goguma/WebSecurityConfig.java
+++ src/main/java/com/example/goguma/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
@@ -30,6 +31,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.headers().frameOptions().disable();
+        http.headers().addHeaderWriter(
+                new StaticHeadersWriter("Access-Control-Allow-Origin", "*"));
         http.authorizeRequests()
                 // 폴더를 login 없이 허용
                 .antMatchers("/js/**").permitAll()
